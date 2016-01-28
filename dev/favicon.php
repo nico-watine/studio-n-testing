3de83c5745d2f7ca356c60dd70dd543afbf81681
Nico Watine <contact@studioncreations.com>
2016-01-28 13:03:54 -0600

    SAFARI v9 Desktop figured out
    At least I’m pretty sure…
    It’s all written out and described on favicon.php

--- dev/favicon.php
+++ dev/favicon.php
@@ -11,8 +11,7 @@
 	<link rel="canonical" href="http://new-project.static" />
 	<link rel="author" href="/humans.txt">
 
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-	<link rel="icon" href="/android-chrome-192x192.png" sizes="192x192">
+	<!-- <link rel="icon" href="/touch-icon-192x192.png" sizes="192x192"> -->
 
 	<meta property="og:locale" content="en_US" />
 	<meta property="og:type" content="website" />
@@ -32,7 +31,15 @@
 </head>
 <body>
 
-
+SAFARI 9<br>
+Seems like for safari desktop v9, just need 'apple-touch-icon.png' and DO NOT need to link it in head. This is what is displayed in the 'favorites' pane, NOT the url bar. <br>
+If you WERE to link it in the head, it would show any image you specify - it does not need to be named 'apple-touch-icon-XXXXX' or any variation of it, be it with 'precomposed', without 'precomposed' or with or without 180x180, etc... <br>
+This is the code to put in HEAD if you WERE to link it :: <br>
+&lt;link rel="apple-touch-icon" href="/apple-touch-icon.png"&gt; <br>
+Note how you have to title it with rel="apple-touch-icon" <br>
+The url bar in RETINA shows the proper favicon.ico, the 32X size. <br>
+the url bar in STANDARD shows the proper favicon.ico, the 16X size <br>
+Need to figure out how to test older Safari versions
 
 
 
