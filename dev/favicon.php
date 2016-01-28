66d8d10dd22b9135c53f19f1060c935f0ef10498
Nico Watine <contact@studioncreations.com>
2016-01-28 11:07:04 -0600

    Added new /dev/favicon.php page

---
+++ dev/favicon.php
@@ -0,0 +1,40 @@
+<?php 
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#" class="no-js" lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<title>Favicon Dev | Studio N Testing</title>
+	<meta name="description" content="Development testing to figure out favicon settings across all browsers &amp; devices.">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="canonical" href="http://new-project.static" />
+	<link rel="author" href="/humans.txt">
+
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	<link rel="icon" href="/android-chrome-192x192.png" sizes="192x192">
+
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Favicon Dev | Studio N Testing" />
+	<meta property="og:description" content="Development testing to figure out favicon settings across all browsers &amp; devices." />
+	<meta property="og:url" content="http://studiontesting.com/dev/favicon" />
+	<meta property="og:site_name" content="Studio N Testing" />
+	<meta property="og:image" content="http://studiontesting.com/img" />
+
+	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>"/>
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
+		<script src="<?php autoVer('/js/ie-min.js'); ?>"></script>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+
+
+
+
+</body>
+</html>
