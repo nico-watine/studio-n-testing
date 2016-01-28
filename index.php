b5509340d28d344a57a49b88cb7f37edc7770261
Nico Watine <contact@studioncreations.com>
2016-01-28 11:00:04 -0600

    Copied over new-project.com template files

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#" class="no-js" lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<title>Page Title | New Project</title>
+	<meta name="description" content="Page description for search results &amp; social shares goes here.">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="canonical" href="http://new-project.static" />
+	<link rel="author" href="/humans.txt">
+
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	<link rel="icon" href="/android-chrome-192x192.png" sizes="192x192">
+
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Page Title" />
+	<meta property="og:description" content="Page description for search results &amp; social shares goes here." />
+	<meta property="og:url" content="http://new-project.static" />
+	<meta property="og:site_name" content="New Project" />
+	<meta property="og:image" content="http://new-project.static/img" />
+
+	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>"/>
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
+		<script src="<?php autoVer('/js/ie-min.js'); ?>"></script>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/vendor/jquery/1-11-3/jquery-min.js"><\/script>')</script>
+<script src="<?php autoVer('/js/scripts-min.js'); ?>"></script>
+
+</body>
+</html>
---
+++ js/src/ie.js
@@ -0,0 +1,4 @@
+// PREPEND ::
+	// /assets/bower_components/REM-unit-polyfill/js/rem.min.js - for em/rem use in IE8
+
+
